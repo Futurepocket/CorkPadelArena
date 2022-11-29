47fb47aa65ff05e4123152461b56620b818d1ea5
Marco Carvalhana <marco.carvalhana@congility.com>
2022-11-29 11:38:29 +0000

    no message

--- functions/node/index.dart
+++ functions/node/index.dart
@@ -1,11 +1,11 @@
-import 'package:firebase_functions_interop/firebase_functions_interop.dart';
-void main() {
-  functions['moin'] = functions
-      //.region('europe-west3')
-      .https.onRequest(greet);
-}
-void greet(ExpressHttpRequest request) {
-  request.response
-    ..writeln('Moin, moin!')
-    ..close();
-}
+// import 'package:firebase_functions_interop/firebase_functions_interop.dart';
+// void main() {
+//   functions['moin'] = functions
+//       //.region('europe-west3')
+//       .https.onRequest(greet);
+// }
+// void greet(ExpressHttpRequest request) {
+//   request.response
+//     ..writeln('Moin, moin!')
+//     ..close();
+// }
